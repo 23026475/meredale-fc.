9c3f3c95945c69d201fa9c308721a7bdd483ef7b
23026475 <ndivhuswiswi@gmail.com>
2026-04-11 15:16:01 +0200

    Update App.jsx: add routes for /team, /players/:id, /coaches/:id

--- meredale-fc/src/App.jsx
+++ meredale-fc/src/App.jsx
@@ -1,27 +1,33 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
-import Players from './pages/Players';
+import Team from './pages/Team';
+import PlayerDetail from './pages/PlayerDetail';
+import CoachDetail from './pages/CoachDetail';
 import Fixtures from './pages/Fixtures';
-import News from './pages/News';
-import Gallery from './pages/Gallery';
-import Contact from './pages/Contact';
 import About from './pages/About';
+import Gallery from './pages/Gallery';
+import Sponsors from './pages/Sponsors';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <div className="min-h-screen flex flex-col">
         <Navbar />
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/players" element={<Players />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/players/:id" element={<PlayerDetail />} />
+            <Route path="/coaches/:id" element={<CoachDetail />} />
             <Route path="/fixtures" element={<Fixtures />} />
-            <Route path="/news" element={<News />} />
-            <Route path="/gallery" element={<Gallery />} />
             <Route path="/about" element={<About />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/sponsors" element={<Sponsors />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
